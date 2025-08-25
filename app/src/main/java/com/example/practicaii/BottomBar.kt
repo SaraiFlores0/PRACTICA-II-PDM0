c026b8f07fc5edaaabdc18cba3ad07e30abfe82d
Saraí Flores <saraiflores302@gmail.com>
2025-08-24 23:57:47 -0600

    Iconos agregados.

--- app/src/main/java/com/example/practicaii/BottomBar.kt
+++ app/src/main/java/com/example/practicaii/BottomBar.kt
@@ -1,5 +1,7 @@
-package com.example.practicaii
-
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle // Ejemplo para "Resumen"
+import androidx.compose.material.icons.filled.List // Ejemplo para "Formulario"
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
@@ -8,19 +10,29 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
-fun BottomBar(navController: NavController){
-    val items = listOf("form" to "Formulario", "summary" to "Resumen")
+fun BottomBar(navController: NavController) {
 
-    NavigationBar{
-        val current = navController.currentBackStackEntryAsState().value?.destination?.route
-        items.forEach { (route, label)->
+    val items = listOf(
+
+        Triple("form", "Formulario", Icons.Filled.List),
+        Triple("summary", "Resumen", Icons.Filled.AccountCircle)
+    )
+
+    NavigationBar {
+        val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+        items.forEach { (route, label, iconVector) ->
             NavigationBarItem(
-                label = { Text(label)},
-                icon = {},
-                selected = current == route,
-                onClick ={
-                    navController.navigate(route){
-                        popUpTo(navController.graph.startDestinationId){
+                label = { Text(label) },
+                icon = {
+                    Icon(
+                        imageVector = iconVector,
+                        contentDescription = label
+                    )
+                },
+                selected = currentRoute == route,
+                onClick = {
+                    navController.navigate(route) {
+                        popUpTo(navController.graph.startDestinationId) {
                             saveState = true
                         }
                         launchSingleTop = true
